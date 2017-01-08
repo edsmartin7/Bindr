f12d78c48eb60413790f2bfe6b117ef50ee7c656
edsmartin7 <edsmartin7@gmail.com>
2017-01-08 15:58:56 -0500

    long time project update, gradle build

--- app/src/main/java/com/example/emartin/bindr/MainActivity.java
+++ app/src/main/java/com/example/emartin/bindr/MainActivity.java
@@ -9,12 +9,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //modified:  pull random images from mysql(?) database
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //setContentView(R.layout.main_fragment);
-        setContentView(R.layout.display_all_profiles);
+        setContentView(R.layout.display_all);
 
 
         //ASync Task
